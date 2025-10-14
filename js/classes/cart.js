54509f516fa7da4a39f417c318415cf1b578e95a
ichiratin123 <phonghuynh1501@gmail.com>
2025-10-14 22:28:08 +0700

    update logic

---
+++ js/classes/cart.js
@@ -0,0 +1,24 @@
+class Cart { 
+    constructor() { 
+        this.mangGioHang = [];
+    }
+
+    themGH(sp) { 
+
+    }
+
+    timViTri(ma) { 
+
+    }
+
+    xoaGH(id) { 
+
+    }
+
+    capNhatSoLuong(id, sl) { 
+
+    }
+}
+
+
+export default Cart;
---
+++ js/main.js
@@ -0,0 +1,3 @@
+import Cart from "./classes/cart.js";
+import CartItem1 from "./classes/cart1.js";
+import CartItem2 from "./classes/cart2.js";
